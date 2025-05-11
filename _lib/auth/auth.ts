3fcf00695e4889e57d97146e29c34131fc6c44cf
Charles ABJ <biadjeu.charles@orange.fr>
2025-05-11 14:30:44 +0200

    last push

--- _lib/auth/auth.ts
+++ _lib/auth/auth.ts
@@ -1,12 +1,16 @@
-export const runtime = 'nodejs'
-
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/_lib/db/prisma"
+import { prisma } from "@/prisma/prisma"
+
+export const runtime = 'nodejs'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [Google],
     debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt",
+    },
+
 })
